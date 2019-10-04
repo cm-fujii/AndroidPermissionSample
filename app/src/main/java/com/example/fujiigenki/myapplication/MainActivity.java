5963a854457e3509ebe212909453eb09f57e66ef
cm-fujii-genki <fujii.genki@classmethod.jp>
2019-10-04 14:49:07 +0900

    許可状態をテキスト表示する

--- app/src/main/java/com/example/fujiigenki/myapplication/MainActivity.java
+++ app/src/main/java/com/example/fujiigenki/myapplication/MainActivity.java
@@ -7,15 +7,23 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private final int REQUEST_LOCATION_PERMISSION = 1000;
 
+    TextView permissionMessage;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        permissionMessage = (TextView)findViewById(R.id.permission_message);
+        permissionMessage.setText("None");
+
 
         // Android 6 (API 23) 以上で、パーミッションを確認する
         if (Build.VERSION.SDK_INT >= 23) {
@@ -28,6 +36,8 @@ public class MainActivity extends AppCompatActivity {
                 Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             // パーミッションが無いのでリクエストする
             requestLocationPermission();
+        } else {
+            permissionMessage.setText("許可されています");
         }
     }
 
@@ -50,8 +60,10 @@ public class MainActivity extends AppCompatActivity {
                 if (grantResults.length > 0 &&
                         grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     // パーミッション許可された
+                    permissionMessage.setText("許可されました");
                 } else {
                     // パーミッション許可されなかった
+                    permissionMessage.setText("許可されませんでした");
                 }
                 break;
             }
